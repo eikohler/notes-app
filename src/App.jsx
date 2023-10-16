6f01ea4ceb8b39686faabfffb235bafefac358bf
Eric Kohler <erickohler1600@gmail.com>
2023-10-16 00:36:09 -0700

    setup column resizer with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.scss';
+import { Container, Section, Bar } from '@column-resizer/react';
 
 function App() {
+
   return (
     <>
       <main>
-
+        <Container className="notes-container">
+          <Section className="notes-list" minSize={200} defaultSize={400} />
+            <Bar size={6} className="resize-bar" />
+          <Section className="note-pad" minSize={200} />
+        </Container>
       </main>
     </>
   );
