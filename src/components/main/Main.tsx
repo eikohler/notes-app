4e58517fd293d3ae648444ba7e42fa7d426bbc4e
Eric Kohler <erickohler1600@gmail.com>
2023-10-16 02:17:30 -0700

    add active class toggle when bar is active with active stylings

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import { Container, Section, Bar, Resizer } from '@column-resizer/react';
 
-function beforeApplyResizer(resizer: Resizer): void {
-  if (resizer.getSectionSize(0) < 100) {
-    resizer.resizeSection(0, { toSize: 0 });
-  } else if (resizer.getSectionSize(0) < 200) {
-    resizer.resizeSection(0, { toSize: 200 });
-  }
-}
-
 function Main() {
+  const [ barActive, setBarActive ] = useState(false);
+
+  const collapseCol = (resizer: Resizer) : void => {
+    if (resizer.getSectionSize(0) < 100) {
+      resizer.resizeSection(0, { toSize: 0 });
+    } else if (resizer.getSectionSize(0) < 200) {
+      resizer.resizeSection(0, { toSize: 200 });
+    }
+  }
+
   return (
-    <Container className="notes-container" beforeApplyResizer={beforeApplyResizer}>
+    <Container 
+      className={`notes-container ${barActive ? "active" : ""}`} 
+      beforeApplyResizer={collapseCol}
+      onActivate={() : void => setBarActive(true)}
+      afterResizing={() : void => setBarActive(false)}
+    >
       <Section className="notes-list" defaultSize={300} />
-        <Bar size={10} className="resize-bar" />
+        <Bar size={5} className="resize-bar" />
       <Section className="note-pad" minSize={300} />
     </Container>
   );
