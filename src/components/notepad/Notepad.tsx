e805aae99b49647ae4ddf24057808c03ea4ccb21
Eric Kohler <erickohler1600@gmail.com>
2023-10-17 00:18:57 -0700

    on first interaction with notepad first line is a h1

--- src/components/notepad/Notepad.tsx
+++ src/components/notepad/Notepad.tsx
@@ -3,20 +3,23 @@ import ReactQuill, { Quill } from "react-quill";
 import 'react-quill/dist/quill.bubble.css';
 import toolBarOptions from "./toolBarOptions";
 
-const BubbleTheme = Quill.import("themes/bubble");
-
+const BubbleTheme = Quill.import("themes/bubble");    
 class ExtendBubbleTheme extends BubbleTheme {
     constructor(quill:any, options:any) {
         super(quill, options);
+        let quillStart = true;
 
         quill.on("selection-change", (range:any) => {            
             if (range) {
-                console.log(quill.getBounds(range));
-                console.log(quill.theme.tooltip.position(quill.getBounds(range)));
+                // console.log(quill.getBounds(range));
+                // console.log(quill.theme.tooltip.position(quill.getBounds(range)));
                 quill.theme.tooltip.show();
                 quill.theme.tooltip.position(quill.getBounds(range));
             }
-            // quill.formatLine(0, 0, 'header', 1);
+            if(quillStart){
+                quillStart = false;
+                quill.formatLine(0, 0, 'header', 1);
+            }
         });
     }
 }
