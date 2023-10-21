61f4b26e614cacfc0d2edaf8f5e3598cfd70793a
Eric Kohler <erickohler1600@gmail.com>
2023-10-20 18:44:39 -0700

    update colorpicker to have a luminance slider

--- src/components/colorpicker/ColorPicker.tsx
+++ src/components/colorpicker/ColorPicker.tsx
@@ -3,19 +3,39 @@ const randColors = require('get-random-colors');
 
 const ColorPicker = () => {
 
-  const [ brightColor, setbrightColor ] = useState((randColors.getRandomColors(69, 1)).hexColorLists);
-  // useEffect(() => {
-  //   // document.getElementById('notePad')!.style.backgroundColor = brightColors[1];
-  // });
+  const [ luminance, setLuminance ] = useState(69);
+  const [ brightColor, setbrightColor ] = useState(getRandomColor);
+
+  function getRandomColor(){
+    return (randColors.getRandomColors(luminance, 1)).hslColorLists[0];
+  }
 
   function updateBGColor(color:any){
     document.body.style.backgroundColor = color;
-    setbrightColor((randColors.getRandomColors(69, 1)).hexColorLists);
+    setbrightColor(getRandomColor);
   }
+
+  const changeLightness = (newLightness:any, hslStr:any) => {
+    const [hue, saturation] = hslStr.match(/\d+/g).map(Number);  
+    return `hsl(${hue}, ${saturation}%, ${newLightness}%)`;
+  };
 
 
   return (
     <div id="color-picker">
+      <div id="luminace-range">
+        <input 
+          type="range"
+          min="1" 
+          max="100"
+          value={luminance}
+          onChange={(event) => {
+            const value = parseInt(event.target.value);
+            setLuminance(value);
+            setbrightColor(changeLightness(value, brightColor));
+          }}
+        />
+      </div>
       <div onClick={() => updateBGColor(brightColor)} className="color-box" style={{backgroundColor: brightColor}}></div>      
     </div>
   )
