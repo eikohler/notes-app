6538422620632d065cf1546069e94acb79398dde
Eric Kohler <erickohler1600@gmail.com>
2023-10-22 22:45:15 -0700

    slider background gradient updates based on luminance value

--- src/components/colorpicker/ColorPicker.tsx
+++ src/components/colorpicker/ColorPicker.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import {changeLightness, hslToHex, getColorLuminance} from '../../helper/helperFunctions';
 const RandColors = require('get-random-colors');
 const { contrastColor } = require('contrast-color');
 
 const ColorPicker = () => {  
+
+  // To update the input slider element
+  const slider = useRef<any>(null);
 
   // Get last used color and luminance from local storage
   // Set luminance to last stored color luminance, if none defaults to 69
@@ -21,12 +24,15 @@ const ColorPicker = () => {
   /* When bg color state is changed:
   Save bg color to local storage
   Update document body's background color
-  Update Text color and resize bar color */
+  Update Text color and resize bar color
+  Update input slider background */
   useEffect(() => {
     localStorage.setItem('bg_color', bgColor);
     document.body.style.backgroundColor = bgColor;
     document.getElementById('notePad')!.style.color = fgColor;
     document.getElementById('resizeBar')!.style.backgroundColor = fgColor;
+    slider.current.style.background = `linear-gradient(90deg, rgba(0, 0, 0, 0.3) ${luminance-4}%, 
+    rgba(255,255,255,0) ${luminance}%)`;
   }, [bgColor]); 
 
   function getRandomColor(){
@@ -38,17 +44,18 @@ const ColorPicker = () => {
 
   // Update state color's luminance value then update text color
   function updateLuminance(event:any){
-    const value = parseInt(event.target.value);
+    const value = parseInt(event.target.value);    
     setLuminance(value);
     setNextBGColor(changeLightness(value, nextBGColor));
     setbgColor(changeLightness(value, bgColor));
     setfgColor(getFGColor);
+
   }
 
   return (
     <div id="color-picker">
       <div id="luminace-range">
-        <input type="range" min="1" max="100" 
+        <input type="range" min="1" max="100" ref={slider}
         value={luminance} onChange={(event) => updateLuminance(event)} />
       </div>
       <div onClick={() => {
