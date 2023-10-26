7b33cd91a56079e4beadf667123c7370edb41239
Eric Kohler <erickohler1600@gmail.com>
2023-10-26 13:04:15 -0700

    update duplicate number display

--- src/components/notelist/Notelist.tsx
+++ src/components/notelist/Notelist.tsx
@@ -8,7 +8,7 @@ const Notelist = (props:any) => {
                 return(
                     <div key={"wrapper-"+note.id}>                   
                         <p key={"title-"+note.id} onClick={() => loadNote(note.id)}>
-                            {note.title}{note.number !== 1 && " ("+note.number+")"}
+                            {note.title}{note.number !== 1 && " "+note.number}
                         </p>
                         <button key={"remove-"+note.id} onClick={()=> deleteNote(note.id)}>Delete</button>
                     </div>
