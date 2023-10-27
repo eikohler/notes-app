601035544d57ddd2e1a92f203c786909bdae7bdf
Eric Kohler <erickohler1600@gmail.com>
2023-10-26 19:38:39 -0700

    update placeholder scrolling change

--- src/components/notepad/Notepad.tsx
+++ src/components/notepad/Notepad.tsx
@@ -5,12 +5,24 @@ import toolBarOptions from "./toolBarOptions";
 
 const Notepad = (props:any) => {
 
+    // For editor placeholder    
+    const namesArr = ['masterpiece', 'story', 'art', 'book', 'song', 'adventure', 'plan', 'day'];
+
     const {noteID, content, updateList} = props;
     const [value, setValue] = useState(content);
+    // const [noteName, setNoteName] = useState(namesArr[Math.floor(Math.random()*namesArr.length)]);
 
     useEffect(() => {
-        setValue(content);
+        setValue(content);        
     }, [noteID]);
+
+    const updatePlaceHolder = (quill:any) =>{        
+        const prevName = quill.root.dataset.placeholderName || 'masterpiece';
+        const newNameArr = namesArr.filter((name:any)=> name !== prevName );
+        const newName = newNameArr[Math.floor(Math.random()*newNameArr.length)]; 
+        quill.root.dataset.placeholderName = newName;
+        quill.root.dataset.placeholder=`your ${newName} ...`;
+    }
 
     const onChange = (newContent: any, delta: any, source: any, editor: any) => {
         setValue(newContent);
@@ -32,16 +44,18 @@ const Notepad = (props:any) => {
         }
     };
 
-    const quill = useCallback((node:any) => {
-        if(node){
-            node = node.getEditor();
-            node.focus();
-            node.formatLine(0, 0, 'header', 1);
+    const quill = useCallback((quill:any) => {
+        if(quill){
+            quill = quill.getEditor();
+            quill.focus();
+            quill.formatLine(0, 0, 'header', 1);
             
-            node.on('editor-change', function() {
-                node.formatLine(0, 0, 'header', 1);
-                if(node.getLength() === 1) node.root.classList.add("ql-blank");                
-            });
+            quill.on('editor-change', function() {
+                quill.formatLine(0, 0, 'header', 1);
+                if(quill.getLength() === 1) quill.root.classList.add("ql-blank");                
+            });                                  
+
+            setInterval(()=>updatePlaceHolder(quill), 1000);
         }
     }, []);
 
@@ -49,8 +63,8 @@ const Notepad = (props:any) => {
         <ReactQuill
             ref={quill}
             theme="bubble"
-            placeholder="..."
             value={value}
+            placeholder={`your masterpiece ...`}
             onChange={onChange}
             modules={{ toolbar: toolBarOptions }}
         />
