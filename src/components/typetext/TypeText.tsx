484b0948b670d45bd3f58465605315edfe9d1b7b
Eric Kohler <erickohler1600@gmail.com>
2023-10-28 05:37:34 -0700

    useEffect getting proper char array, plays first title twice

--- src/components/typetext/TypeText.tsx
+++ src/components/typetext/TypeText.tsx
@@ -15,31 +15,39 @@ const TypeText = () => {
     const [chars, setChars] = useState(textArr[index].split(''));        
     const [animClass, setAnimClass] = useState('');
     const [initialWait, setInitialWait] = useState(800);
+    const buffer = 1;
 
-    useEffect(() => {
-        setChars(textArr[index].split(''));
-        setInitialWait(0);
-        setAnimClass('');
-        
-        const delay = ((chars.length)*0.2)*1000;
+    useEffect(() => {        
+        const delay = ((0.1*chars.length)+buffer)*1000;
 
-        setTimeout(function(){
+        // Current Problem: Plays first title twice
+
+        console.log(delay);
+
+        const timer = setTimeout(function(){
             setAnimClass('anim-in');
             setTimeout(function(){
                 setAnimClass('anim-out');
                 setTimeout(function(){
-                    setIndex(index + 1 < textArr.length ? index + 1 : 0);                    
+                    setIndex(index + 1 < textArr.length ? index + 1 : 0);
+                    setChars(textArr[index].split(''));
+                    setInitialWait(0);
+                    setAnimClass('');
                 }, delay);
             }, delay);
-        }, initialWait);        
+        }, initialWait);  
         
-    }, [index]);
+        return () => {            
+            clearTimeout(timer);
+        };
+        
+    }, [chars]);
 
     return (
-        <div id="ph-text" className={animClass}>{
+        <div id="ph-text" className={animClass}>{            
             chars.map((char:any, i:any)=>{return(
                 <span style={{
-                    animationDelay: animClass == ('' || 'anim-in') ? ((i+1)*0.1)+'s' : ((chars.length-i)*0.1)+'s'
+                    animationDelay: animClass == ('' || 'anim-in') ? (i*0.1)+'s' : (((chars.length-1)-i)*0.1)+'s'
                 }} 
                 key={char+i}>{char}</span>
             )})
