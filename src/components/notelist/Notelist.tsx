2f63150c1e506f8a506c9a31d083164716389740
Eric Kohler <erickohler1600@gmail.com>
2023-11-05 23:48:02 -0800

    drag reorder smooth transition with floating element

--- src/components/notelist/Notelist.tsx
+++ src/components/notelist/Notelist.tsx
@@ -7,7 +7,8 @@ const Notelist = (props:any) => {
     const {noteID, noteList, loadNote, deleteNote, newOrderList} = props;    
     const [hoverIndex, setHoverIndex] = useState(-1);
     const [dragIndex, setDragIndex] = useState(-1);
-    const [dragOverIndex, setDragOverIndex] = useState(-1);    
+    const [dragOverIndex, setDragOverIndex] = useState(-1);
+    const [activeIndex, setActiveIndex] = useState(-1);
 
     const mousePosition = useMousePosition();
 
@@ -20,11 +21,11 @@ const Notelist = (props:any) => {
             newOrderList(newList);
             setDragIndex(dragOverIndex);
         }        
-    }, [dragOverIndex]);
+    }, [dragOverIndex]);      
 
-    useEffect(() => {
-        console.log(dragIndex);
-    }, [dragIndex]);
+    useEffect(() => {        
+        setActiveIndex(noteList.findIndex((note:any) => note.id === noteID));
+    }, [noteID, noteList]);      
 
     return (
         <>
@@ -32,19 +33,19 @@ const Notelist = (props:any) => {
                 const hover = hoverIndex === i;
                 const hovering = hoverIndex !== -1;
                 const active = noteID === note.id;
-                const isDragItem = dragIndex === i;
                 const isDragging = dragIndex !== -1;
+
                 return(
                     <div onClick={() => loadNote(note.id)}
                     key={"wrapper-"+note.id} 
                     className={`note-wrapper 
-                        ${(active && (!hovering || isDragging)) ? 'active' : ''} 
-                        ${isDragItem ? 'dragging' : ''}
-                        ${isDragging ? 'no-hover' : ''}
+                        ${(active && (!hovering || isDragging)) ? 'active' : ''}
+                        ${isDragging ? 'dragging' : ''}
                     `}
                     onDragStart={(e) => {e.preventDefault(); setDragIndex(i); loadNote(note.id);}}
                     draggable                
-                    onMouseEnter={() => {
+                    onMouseEnter={(e) => {
+                        console.log(e.target);
                         if(dragIndex !== -1){
                             setDragOverIndex(i);
                             setHoverIndex(-1);
@@ -55,19 +56,26 @@ const Notelist = (props:any) => {
                     onMouseUp={()=>{setDragIndex(-1); setDragOverIndex(-1);}}
                     onMouseLeave={() => setHoverIndex(-1)}
                     style={{
-                        backgroundColor: hover || active
-                        ? note.colors.bgColor 
-                        : '#2a2b2a',
-
-                        color: hover || active 
-                        ? note.colors.fgColor 
-                        : '#fff'
+                        backgroundColor: hover || active ? note.colors.bgColor : '#2a2b2a',
+                        color: hover || active ? note.colors.fgColor : '#fff',
                     }}>
                         <p key={"title-"+note.id} className="title">{note.title}</p>
                         <div className="text-content" key={"text"+note.id}>{parse(note.text)}</div>
                     </div>
                 )
             })}
+            {activeIndex !== -1 && (
+                <div className={`note-wrapper drag-note ${dragIndex !== -1 ? 'move' : ''}`}
+                style={{
+                    backgroundColor: noteList[activeIndex].colors.bgColor,
+                    color: noteList[activeIndex].colors.fgColor,
+                    // left: mousePosition.x!,
+                    top: mousePosition.y!
+                }}>
+                    <p className="title">{noteList[activeIndex].title}</p>
+                    <div className="text-content">{parse(noteList[activeIndex].text)}</div>
+                </div>
+            )}
         </>
     )
 }
