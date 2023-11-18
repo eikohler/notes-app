216b836ae81356e9fd68957576de967327df3929
Eric Kohler <erickohler1600@gmail.com>
2023-11-18 04:35:13 -0500

    enablescroll on main

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { useEffect } from 'react';
 
 const bodyScrollLock = require('body-scroll-lock');
 const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 function App() {
 
   useEffect(() => {
     const body = document.querySelector('body');
+    const main = document.querySelector('main');
     disableBodyScroll(body);
+    enableBodyScroll(main);
   }, []);
 
   return (    
