447e6da2dfacbed56ef0a6ea3b78857ee7b6ac79
Eric Kohler <erickohler1600@gmail.com>
2023-11-18 04:37:10 -0500

    disable body scroll lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.scss';
 import Main from './components/main/Main';
-import { useEffect } from 'react';
 
-const bodyScrollLock = require('body-scroll-lock');
-const disableBodyScroll = bodyScrollLock.disableBodyScroll;
-const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+// import { useEffect } from 'react';
+// const bodyScrollLock = require('body-scroll-lock');
+// const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+// const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 function App() {
 
-  useEffect(() => {
-    const body = document.querySelector('body');
-    const main = document.querySelector('main');
-    disableBodyScroll(body);
-    enableBodyScroll(main);
-  }, []);
+  // useEffect(() => {
+  //   const body = document.querySelector('body');
+  //   const main = document.querySelector('main');
+  //   disableBodyScroll(body);
+  //   enableBodyScroll(main);
+  // }, []);
 
   return (    
     <Main />    
