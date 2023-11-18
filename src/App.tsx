3d8213d833b5251d25dace3e48976e99581987a0
Eric Kohler <erickohler1600@gmail.com>
2023-11-18 04:28:11 -0500

    import body scroll lock to disable body scroll on all devices and ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.scss';
 import Main from './components/main/Main';
+import { useEffect } from 'react';
+
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
 
 function App() {
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    disableBodyScroll(body);
+  }, []);
+
   return (    
     <Main />    
   );
