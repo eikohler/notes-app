c4eb6c37ca1807bf2348c47360e9b5f970f9ebc9
Eric Kohler <erickohler1600@gmail.com>
2023-11-18 04:46:58 -0500

    remove body scroll lock package, adjust drag auto scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import './App.scss';
 import Main from './components/main/Main';
 
-// import { useEffect } from 'react';
-// const bodyScrollLock = require('body-scroll-lock');
-// const disableBodyScroll = bodyScrollLock.disableBodyScroll;
-// const enableBodyScroll = bodyScrollLock.enableBodyScroll;
-
 function App() {
-
-  // useEffect(() => {
-  //   const body = document.querySelector('body');
-  //   const main = document.querySelector('main');
-  //   disableBodyScroll(body);
-  //   enableBodyScroll(main);
-  // }, []);
-
   return (    
     <Main />    
   );
